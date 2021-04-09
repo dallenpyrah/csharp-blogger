928941574b3a7dc72890ef31f6e0bc58c6ba7d86
Dallen Pyrah <dallenpyrah@gmail.com>
2021-04-09 15:56:56 -0600

    Blogs are done

--- Setup.sql
+++ Setup.sql
@@ -10,17 +10,22 @@ USE bloggerdallen;
 --     PRIMARY KEY (id)
 -- )
 
--- CREATE TABLE blogs
--- (
---     id INT NOT NULL AUTO_INCREMENT,
---     title VARCHAR(255) NOT NULL,
---     body VARCHAR(255),
---     imgUrl VARCHAR(255),
---     published BOOLEAN,
---     creatorId VARCHAR(255) NOT NULL,
 
---     PRIMARY KEY (id)
--- )
+CREATE TABLE theblogs
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    body VARCHAR(255),
+    imgUrl VARCHAR(255),
+    published BOOLEAN,
+    creatorId VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (id),
+
+    FOREIGN KEY (creatorId)
+    REFERENCES profiles (id)
+    ON DELETE CASCADE
+)
 
 -- CREATE TABLE comments
 -- (
@@ -34,5 +39,4 @@ USE bloggerdallen;
 --     FOREIGN KEY (blogId)
 --         REFERENCES blogs (id)
 --         ON DELETE CASCADE
-
 -- );
